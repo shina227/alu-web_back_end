4f8beec38a7bd2527ac152a3cadd37aaa69e1795
Shina Atete Mpeta <atetempeta@gmail.com>
2026-05-09 21:49:52 +0200

    tasks

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""This module contains a function that adds two float numbers together."""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Takes two float numbers a and b and returns their sum as a float.
+    Parameters:
+    a (float): The first number to be added.
+    b (float): The second number to be added.
+    Returns:
+    float: The sum of the two numbers.
+    """
+    return a + b
---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+Basic concat that takes a string str1 and a string str2 as arguments
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    A function that returns a concatenated string
+    """
+    return str1 + str2
