c36892fe819f4ce4cb7aacd022f749e01cdac5b3
Shina Atete Mpeta <atetempeta@gmail.com>
2026-06-11 10:26:30 +0200

    tests

---
+++ Unittests_and_integration_tests/utils.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import requests
+from functools import wraps
+from typing import Mapping, Sequence, Any, Dict, Callable
+
+__all__ = [
+    "access_nested_map",
+    "get_json",
+    "memoize",
+]
+
+def access_nested_map(nested_map: Mapping, path: Sequence) -> Any:
+    """Access nested map with key path."""
+    for key in path:
+        if not isinstance(nested_map, Mapping):
+            raise KeyError(key)
+        nested_map = nested_map[key]
+    return nested_map
+
+def get_json(url: str) -> Dict:
+    """Get JSON from remote URL."""
+    response = requests.get(url)
+    return response.json()
+
+def memoize(fn: Callable) -> Callable:
+    """Decorator to memoize a method."""
+    attr_name = "_{}".format(fn.__name__)
+
+    @wraps(fn)
+    def memoized(self):
+        if not hasattr(self, attr_name):
+            setattr(self, attr_name, fn(self))
+        return getattr(self, attr_name)
+
+    return property(memoized)
